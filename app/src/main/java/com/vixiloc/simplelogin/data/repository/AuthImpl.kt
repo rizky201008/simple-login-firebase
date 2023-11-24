1b03855e205506cc9e95dcbe2002b90389256c67
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-11-24 23:53:51 +0700

    modify AuthImpl.kt

--- app/src/main/java/com/vixiloc/simplelogin/data/repository/AuthImpl.kt
+++ app/src/main/java/com/vixiloc/simplelogin/data/repository/AuthImpl.kt
@@ -1,21 +1,24 @@
 package com.vixiloc.simplelogin.data.repository
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.vixiloc.simplelogin.data.remote.dto.LoginResponseDto
 import com.vixiloc.simplelogin.domain.data.UserCredential
 import com.vixiloc.simplelogin.domain.repository.Auth
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class AuthImpl(private val auth: FirebaseAuth) : Auth {
     override suspend fun login(user: UserCredential): LoginResponseDto {
-        var message = ""
-        auth.signInWithEmailAndPassword(user.email!!, user.password!!)
-            .addOnCompleteListener { task ->
-                message = if (task.isSuccessful) {
-                    "success"
-                } else {
-                    "failed"
+        return suspendCoroutine { continuation ->
+            auth.signInWithEmailAndPassword(user.email!!, user.password!!)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        continuation.resume(LoginResponseDto(message = "success"))
+                    } else {
+                        continuation.resume(LoginResponseDto(message = "error"))
+                    }
                 }
-            }
-        return LoginResponseDto(message = message)
+        }
     }
 }
